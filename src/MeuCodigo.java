272eff2f0779fac56792c584fa6b6397ed26ed8c
geovanamedeiro <geovanamedeiro7@gmail.com>
2022-11-06 19:29:11 -0300

    first commit

---
+++ src/MeuCodigo.java
@@ -0,0 +1,12 @@
+public class MeuCodigo {
+    public static void main (String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("BOM DIA ");
+        System.out.println("Boa tarde");
+        System.out.println("boa noite");
+        int y = 32;
+        System.out.print(y);
+        double x = 10.34567;
+        System.out.printf("%2f%n", x);
+    }
+}
