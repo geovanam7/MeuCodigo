f38f21cd3cd4e66409c3bdfa26cd9b3c3c3a8ab0
geovanamedeiro <geovanamedeiro7@gmail.com>
2022-11-10 15:00:26 -0300

    2 coomit

--- src/BoletimEscolar.java
+++ src/BoletimEscolar.java
@@ -6,10 +6,26 @@ public class BoletimEscolar {
         if (mediaFinal == 6);
 
         System.out.println ("REPROVADO");
-            if (mediaFinal==6){
-                System.out.println ("PROVA MINERVA");
+        if (mediaFinal==6){
+            System.out.println ("PROVA MINERVA");
+        }
 
-            }
+
+
+
+
+        int a, b;
+            a= 5;
+            b= 6;
+
+            String resultado = "";
+            if (a==b)
+                resultado= "verdadeiro";
+            else
+                resultado = "falso";
+
+            boolean b
+
 
 
     }
